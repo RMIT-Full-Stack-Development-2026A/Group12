f63c0f8e27a65e42ab4dc620dfd5be0a2156407b
Nguyen10220 <136629257+Nguyen10220@users.noreply.github.com>
2026-04-07 23:16:43 +0700

    Update App.jsx

--- full-stack-group-assignment/src/App.jsx
+++ full-stack-group-assignment/src/App.jsx
@@ -105,7 +105,7 @@ function App() {
                 >
                   <use href="/icons.svg#bluesky-icon"></use>
                 </svg>
-                Bluesky
+                
               </a>
             </li>
           </ul>
